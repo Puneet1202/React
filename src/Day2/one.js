31738ca18bad207992b27083145f165f1c0406f2
Puneet1202 <punitkumar2121999@gmail.com>
2024-09-15 14:13:46 +0530

    new folder add

---
+++ src/Day2/one.js
@@ -0,0 +1,17 @@
+// function One(){
+//     return({
+//         name: "One",
+//         age : "25"
+//     });
+// }
+function One() { 
+
+    return (
+    <div>
+            <h1>One</h1>
+            
+    </div>
+    );
+}
+  
+export default One;
