f696c41402a9af0ca6a4173633608da43971e821
Puneet1202 <punitkumar2121999@gmail.com>
2024-09-15 14:24:17 +0530

    remove import day2 folder file one

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import '../src/page1.css';
 import Page1 from './files/page1';
 import Page2 from './files/page2';
 import Page3 from './files/page3';
-import One from   './Day2/one';
 
 function App() {
   return (<>
   <Page1></Page1>
   <Page2></Page2>
   <Page3></Page3>
-  <One></One>
+
 
   </>
     
